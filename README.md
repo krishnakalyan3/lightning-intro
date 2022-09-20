9b0cac0edd4e2539bbb942714134e01d040a778f
krishnakalyan3 <krishnakalyan3@gmail.com>
2022-09-20 12:11:30 +0200

    command

--- README.md
+++ README.md
@@ -17,3 +17,20 @@ python -m lightning run app app.py --open-ui false
 # Running App on the cloud 
 python -m lightning run app app.py --cloud --open-ui false
 ```
+
+### API
+Try out the swagger API. Add a command using `/command/add`
+
+https://*.litng.ai/docs
+
+### Connect
+
+```
+lightning connect 01gdd5e39n6bvx8thv9sy3p3re 
+lightning add --cmd=hello
+lightning disconnect
+```
+
+### Links
+- [Getting Started](https://lightning.ai/lightning-docs/get_started/lightning_apps_intro.html)
+- [CLI Server](https://lightning.ai/lightning-docs/workflows/build_command_line_interface/cli.html?highlight=cli)
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import lightning as L
 
-class SingleWorkFlow(L.LightningWork):
+class HelloWork(L.LightningWork):
     def __init__(self):
         super().__init__()
 
@@ -11,21 +11,21 @@ class SingleWorkFlow(L.LightningWork):
 class RootFlow(L.LightningFlow):
     def __init__(self):
         super().__init__()
-        self.single_work = SingleWorkFlow()
-        self.names = []
+        self.hello_work = HelloWork()
+        self.command = None
 
-    def add_name(self, name: str):
-        print(f"Received name: {name}")
-        self.names.append(name)
+    def add_command(self, cmd: str):
+        print(f"Received cmd: {cmd}")
+        self.command = cmd
 
     def configure_commands(self):
         commands = [
-            {"add": self.add_name},
+            {"add": self.add_command},
         ]
         return commands
 
     def run(self):
-        if "addverb" in self.names:
-            self.single_work.run()
+        if self.command == "hello":
+            self.hello_work.run()
 
 app = L.LightningApp(RootFlow())
